2901e2fc643db86a48fef99b1793bd8d56ea73ec
Mohamadou <mohamadoumamoudou997@gmail.com>
2026-04-20 20:35:12 +0200

    ajout fonction ajouter_matiers

--- scripts/matieres.py
+++ scripts/matieres.py
@@ -11,8 +11,24 @@ MATIERES = {
     'BAC' : {
         'C' : ['Mathematiques', 'Physique', 'Chimie', 'Informatique'],
         'D' : ['SVT', 'Mathematiques', 'Physique', 'Chimie', 'Informatique'],
-        'A4' : ['Literature', 'Langue', 'Anglais', ]
-    },
+        'A4' : ['Literature', 'Langue', 'Anglais', ],
 }
-print(MATIERES['BEPC'])
-print(MATIERES['BAC']['C'])
+}
+def afficher_matieres(niveau,serie=None):
+    if niveau == 'BEPC':
+        print(f"\nBEPC - Matieres:")
+        for matiere in MATIERES['BEPC']:
+            print(f"  -{matiere}")
+    else:
+        if serie:
+            print(f"\n{niveau} Serie {serie}  -Matieres :")
+            for matiere in MATIERES[niveau][serie]:
+                print(f"  -{matiere}")
+        else:
+            for s, matiere in MATIERES[niveau].items():
+                print(f" serie {s} :")
+                for matieres in MATIERES[niveau][serie]:
+                    print(f"  -{matiere}")
+afficher_matieres('BEPC')
+afficher_matieres('BAC', 'C')
+afficher_matieres('PROBATOIRE', 'D')
