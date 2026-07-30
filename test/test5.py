e8c15ff5a0544e06f054c29ac09401fa2c832554
Mohamadou <mohamadoumamoudou997@gmail.com>
2026-07-30 22:44:07 +0200

    Ignorer le dossier test

--- test/test5.py
+++
@@ -1,4 +0,0 @@
-# test_slug_probatoire2.py
-from database_carrefour import get_carrefour
-import json
-print(json.dumps(get_carrefour("Probatoire", "Mathematiques", serie="C"), indent=2, ensure_ascii=False))
